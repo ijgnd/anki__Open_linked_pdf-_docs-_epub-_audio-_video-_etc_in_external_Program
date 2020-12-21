eb4fdcdcdc1135e89217af95591feb6ce937cfcc
ijgnd <fooijgnd@barijgnd>
2020-12-22 00:13:56 +0100

    tweak gc

--- src/config.py
+++ src/config.py
@@ -2,11 +2,12 @@ import os
 
 from aqt import mw
 
+
 def gc(arg, fail=False):
     try:
         out = mw.addonManager.getConfig(__name__).get(arg, fail)
     except:
-        return None
+        return False
     else:
         return out
 
